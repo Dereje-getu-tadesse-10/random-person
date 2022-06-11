b8ce3b34e5a72b55d433b4f533f58b233a55883d
Dereje <getutadessedereje@icloud.com>
2022-06-11 13:26:04 +0200

    Update : ajout image avec l'api

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,24 @@ const generatedLastName = document.querySelector<HTMLParagraphElement>('.last-na
 const generatedEmail = document.querySelector<HTMLParagraphElement>('.email');
 const genratedLocation = document.querySelector<HTMLParagraphElement>('.location');
 const generatedPhone = document.querySelector<HTMLParagraphElement>('.number-phone');
+const generatedImage = document.querySelector<HTMLImageElement>('.avatar');
 const generatePerson = document.querySelector<HTMLButtonElement>('.btn-generate');
+
+
+// get avantar human from api
+function getAvatar(url : string) {
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        const avatar : string = data.avatar;
+        console.log(avatar);
+        document.querySelector<HTMLImageElement>('.avatar').src = avatar;
+    })
+}
+
+
+
 // random person api
 const url : string = 'https://randomuser.me/api/';
 
@@ -13,14 +30,14 @@ function getRandomPerson(url : string) {
     .then(data => {
 
         // destructuring data
-        const {name, lastname, email, location, phone} = data.results[0];
-        
+        const {picture, name, email, location, phone} = data.results[0];
+
         generatedName.textContent = `First name : ${name.first}`;
         generatedLastName.textContent = `Last name : ${name.last}`;
         generatedEmail.textContent = `Email : ${email}`;
         genratedLocation.textContent = `Location : ${location.city}`;
         generatedPhone.textContent = `Phone : ${phone}`;
-
+        generatedImage.src = picture.large;
     })
 }
 
